698b5b14fd011fce9d00fe6d2185ded2ee384205
developerazadpro <azharul.ece13.hstu@gmail.com>
2026-01-08 13:05:12 +0600

    feat(auth): add user authentication and role management

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -40,6 +41,9 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $clientRole = Role::where('name', 'client')->first(); // New user becomes a client by default
+        $user->roles()->attach($clientRole);
 
         event(new Registered($user));
 
