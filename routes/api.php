f114eafe7fa356e3189abcc06bb3f6c859d3380f
developerazadpro <azharul.ece13.hstu@gmail.com>
2026-01-15 18:04:47 +0600

    login api and refactor work job

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
-use App\Http\Controllers\WorkJobController;
+use App\Http\Controllers\API\WorkJobController;
+use App\Http\Controllers\API\LoginController;
 use Illuminate\Support\Facades\Route;
 
+// PUBLIC (no token)
+Route::post('/login', [LoginController::class, 'login']);
+
+// PROTECTED (token required)
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('/work-jobs', [WorkJobController::class, 'index']);
     Route::post('/work-jobs', [WorkJobController::class, 'store']);
